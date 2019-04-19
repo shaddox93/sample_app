a53c0be23053d78cea617e06bafd71370f434eaa
shaddox93 <haddoxshannon93@gmail.com>
2019-04-18 23:18:29 -0400

    rails flavored ruby commit

---
+++ playground.rb
@@ -0,0 +1,23 @@
+# Returns the full title on a per-page basis.
+def full_title (page_title = '')
+
+# printing a string using puts will add a new line
+puts "foo"
+
+# printing a string using print will not
+print "foo"
+
+# interpolation
+first_name = "Shannon"
+last_name = "Stasek"
+#{first_name} #{last_name}
+
+# ruby will not interpolate into single quoted srings
+'#{foo} bar'
+>> "\#{foo} bar"
+
+# boolean methods are indicated by ?
+# "".empty? returns true
+# "test".empty? returns false
+
+end
